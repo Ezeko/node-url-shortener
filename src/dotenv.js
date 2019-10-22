5a54a95497ced2e8fd8323848cd1e1709bb3450b
Adejobi Ezekiel <ezeko2017@gmail.com>
2019-10-22 21:11:13 +0100

    pulled upstream repo

--- src/dotenv.js
+++ src/dotenv.js
@@ -1,16 +1,15 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 PORT=8300
 DB_URL='mongodb://shonubijerry:password@localhost:27017/trim'
 DOMAIN_NAME='localhost:8300'
 SECRET_KEY='my_secrete_key'
 SAMPLE_COOKIE='4n-hnqI6iBIQo8-pyu2yW'
-export const {
-  PORT,
-  DB_URL,
-  DOMAIN_NAME,
-  SECRET_KEY,
-  SAMPLE_COOKIE
-} = process.env;
+
+const dotenv = {
+    PORT,
+    DB_URL,
+    DOMAIN_NAME,
+    SECRET_KEY, 
+    SAMPLE_COOKIE
+}
+
+module.exports = dotenv;
